3c755623790f341d342e8a14e78ca977377bccc7
Tom Nightingale <tom@weareleague.co.uk>
2021-11-17 18:53:35 +0000

    fix menu scroll

--- components/MobileMenu.js
+++ components/MobileMenu.js
@@ -37,7 +37,7 @@ export default function mobileMenu() {
             </button>
 
             {/* Mobile Menu */}
-            <div open={open} className={`fixed top-0 left-0 -z-10 w-full min-h-screen p-4 transition-all duration-200 bg-white ${open ? 'opacity-100 z-40' : 'opacity-0'}`}>
+            <div open={open} className={`fixed top-0 left-0 -z-10 w-full min-h-screen p-4 transition-all duration-200 bg-white ${open ? 'opacity-100 z-40' : 'opacity-0'}`} data-scroll-sticky data-scroll-target="#Home">
                 
                 <div className="flex flex-col items-center justify-center min-h-screen mt-[-50px] sm:mt-0">
 
