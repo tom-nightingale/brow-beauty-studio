2ede8d521ac0ad6a702002df612192374e60464a
Tom Nightingale <tomnightingale@thisissoon.com>
2023-01-07 07:53:32 +0000

    fix(Build): Fix build issue with import

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -12,7 +12,7 @@ import Where from 'components/Where'
 import Contact from 'components/Contact'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
-import Card from '@/components/card';
+import Card from '@/components/Card';
 import Pagination from '@/components/pagination'
 
 export default function Blog({ subscription }) {
